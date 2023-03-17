d506c2849e4557287693df0629266d5fdb5b15f9
kevin.argumedo <kevin.argumedo@gmail.com>
2023-03-17 16:09:58 -0500

    Nav bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: ["index.html"],
   theme: {
-    extend: {
-      fontFamily:{
-        'Outfit':['sans-serif']
-      },
-      colors:{
-        'LightGray':' hsl(212, 45%, 89%)',
-        'GrayishBlue':'hsl(220, 15%, 55%)',
-        'DarkBlue':'hsl(218, 44%, 22%)'
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
